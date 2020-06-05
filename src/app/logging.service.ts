12e80faa225f1337a9cfc24869cc92f8db0df9c4
Tyler Horiuchi <tylerkehoriuchi@gmail.com>
2020-06-05 11:18:53 -0600

    finished practice module

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,5 +1,7 @@
+import { Injectable } from "@angular/core";
 
-
+// it's good practice to add this if services are being injected into one another
+@Injectable()
 export class LoggingService {
     logStatusChange(status: string) {
         console.log('A server status changed, new status' + status);
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,23 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, Output } from '@angular/core';
+
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService,
+              private accountService: AccountService) {
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
 
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
